ce3209653e6ceea330da87dc25faa8f617dd44bb
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-10-11 05:46:18 -0400

    paso de datos entre request y response con filtros globales
    Ejemplo de paso de datos entre request y response con filtros globales, ademas de crear un orden de ejecucion del filtro.

--- src/main/java/com/ms/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/ms/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -1,9 +1,12 @@
 package com.ms.springboot.app.gateway.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,7 +15,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class EjemploGlobalFilter implements GlobalFilter{
+public class EjemploGlobalFilter implements GlobalFilter, Ordered{
 
 	private static Logger logger = LoggerFactory.getLogger(EjemploGlobalFilter.class);
 	
@@ -20,13 +23,31 @@ public class EjemploGlobalFilter implements GlobalFilter{
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		// TODO Auto-generated method stub
 		logger.info("EJECUTANDO FILTRO PRE");
+		
+		//adicionando un token en el request
+		exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
+		
 		return chain.filter(exchange).then(Mono.fromRunnable(()->{
 			logger.info("EJECUTANDO FILTRO POST");
+			
+			//adicionando token al responde obtenido desde el request
+			//ejemplo con Optional pero puede ser con un If
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).
+			ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+			
 			exchange.getResponse().getCookies().
 			add("color", ResponseCookie.from("color", "rojo").build());
 			
 			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return -1;
 	}
 
 }
