a4f8c6ef4cbfd4cc09cf575775dbbb4fa303d918
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-11-14 10:25:57 -0500

    usando resilience4j, circuitbreaker

--- src/main/java/com/ms/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ src/main/java/com/ms/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
